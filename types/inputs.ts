0a07c089317f4d71cb8a88175cf746b2aa99b8d0
David <nundavid99@gmail.com>
2024-05-12 15:59:38 -0400

    corrected input typing

--- types/inputs.ts
+++ types/inputs.ts
@@ -1,11 +1,33 @@
-import { StyleProp, ViewStyle } from "react-native";
+import { DimensionValue, StyleProp, ViewStyle } from "react-native";
 
 export type Input = {
   containerStyle?: StyleProp<ViewStyle>;
-  placeholder: string;
+  placeholder?: string;
   onChange?: (name: string, value: string) => void;
   error?: string;
   name: string;
   label: string;
   value?: string;
+  width?:
+    | DimensionValue
+    | "$px"
+    | "$0"
+    | "$0.5"
+    | "$1"
+    | "$1.5"
+    | "$2"
+    | "$2.5"
+    | "$3"
+    | "$3.5"
+    | "$4"
+    | "$4.5"
+    | "$5"
+    | "$6"
+    | "$7"
+    | "$8"
+    | "$9"
+    | "$10"
+    | "$11"
+    | "$12"
+    | "$16";
 };

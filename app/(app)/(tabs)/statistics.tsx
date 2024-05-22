7186136f1b39d37f0de9cdb2bddb69755302e71c
Fer Rios <luis.fer334.fa@gmail.com>
2024-05-22 16:33:48 -0400

    sin terminar

--- app/(app)/(tabs)/statistics.tsx
+++ app/(app)/(tabs)/statistics.tsx
@@ -1,18 +1,32 @@
 import { StyleSheet } from "react-native";
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
+import { RadarChart } from "@/components/statistcs/RadarChart";
+import { PieChart } from "@/components/statistcs/PieChart";
+import { ScrollView } from "@gluestack-ui/themed";
+import { BarChart } from "@/components/statistcs/BarChart";
+import { LineChart } from "@/components/statistcs/LineChart";
+import { generateFakePoints } from "@/utils";
+
 export default function TabTwoScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Estadisticas</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>% de acierto por habilidad</Text>
+      <PieChart />
+      <Text style={styles.title}>Áreas entrenadas / entrenamiento total</Text>
+      <RadarChart />
+      <Text style={styles.title}>Respuestas / RPE</Text>
+      <BarChart />
+      <Text style={styles.title}>Evolución de actividades</Text>
+      <LineChart linesData={activitiesData} />
+    </ScrollView>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
   title: {
     fontSize: 20,
@@ -24,3 +38,11 @@ const styles = StyleSheet.create({
     width: "80%",
   },
 });
+
+const activitiesData = {
+  "Toma de decisión": generateFakePoints(),
+  Memoria: generateFakePoints(),
+  Reconocimiento: generateFakePoints(),
+  "T.D. + Memoria": generateFakePoints(),
+  Random: generateFakePoints(),
+};

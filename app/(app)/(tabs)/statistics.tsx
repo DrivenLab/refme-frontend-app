ba040ec790e9915ec1dff58e1faaa834be21c623
Fer Rios <luis.fer334.fa@gmail.com>
2024-05-23 21:49:06 -0400

    add styles to radarchart

--- app/(app)/(tabs)/statistics.tsx
+++ app/(app)/(tabs)/statistics.tsx
@@ -14,11 +14,20 @@ export default function TabTwoScreen() {
       <Text style={styles.title}>% de acierto por habilidad</Text>
       <PieChart />
       <Text style={styles.title}>Áreas entrenadas / entrenamiento total</Text>
-      <RadarChart />
+      <RadarChart labels={radarLabels} characterData={characterData} />
       <Text style={styles.title}>Respuestas / RPE</Text>
       <BarChart />
       <Text style={styles.title}>Evolución de actividades</Text>
       <LineChart linesData={activitiesData} />
+      <Text style={styles.title}>Evolución de toma de decisión</Text>
+      <LineChart linesData={decisionData} />
+      <Text style={styles.title}>Evolución de tiempo de respuesta</Text>
+      <LineChart
+        linesData={timeResponseData}
+        tickFormat={(v) => `${v} s`}
+        yAxis={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+        domain={{ y: [0, 10] }}
+      />
     </ScrollView>
   );
 }
@@ -46,3 +55,77 @@ const activitiesData = {
   "T.D. + Memoria": generateFakePoints(),
   Random: generateFakePoints(),
 };
+
+const decisionData = {
+  Manos: generateFakePoints(),
+  "Faltas tácticas": generateFakePoints(),
+  Disputas: generateFakePoints(),
+};
+
+const timeResponseData = {
+  "Toma de decisión": [
+    { x: 1, y: 6 },
+    { x: 2, y: 5 },
+    { x: 3, y: 6 },
+    { x: 4, y: 5 },
+    { x: 5, y: 6 },
+    { x: 6, y: 5 },
+  ],
+  Memoria: [
+    { x: 1, y: 5 },
+    { x: 2, y: 4 },
+    { x: 3, y: 5 },
+    { x: 4, y: 4 },
+    { x: 5, y: 5 },
+    { x: 6, y: 4 },
+  ],
+  Reconocimiento: [
+    { x: 1, y: 4 },
+    { x: 2, y: 3 },
+    { x: 3, y: 4 },
+    { x: 4, y: 3 },
+    { x: 5, y: 4 },
+    { x: 6, y: 3 },
+  ],
+  "T.D. + Memoria": [
+    { x: 1, y: 3 },
+    { x: 2, y: 2 },
+    { x: 3, y: 3 },
+    { x: 4, y: 2 },
+    { x: 5, y: 3 },
+    { x: 6, y: 2 },
+  ],
+  Random: [
+    { x: 1, y: 2 },
+    { x: 2, y: 1 },
+    { x: 3, y: 2 },
+    { x: 4, y: 1 },
+    { x: 5, y: 2 },
+    { x: 6, y: 1 },
+  ],
+};
+
+const radarLabels = [
+  "2O reps. 80% / T 3,4s",
+  "2O reps. 40% / T 3,4s",
+  "18 reps. 60% / T 3,4s",
+  "12 reps. 40% / T 3,4s",
+  "2O reps. 90% / T 3,4s",
+];
+
+const characterData = [
+  {
+    "Toma de Decisión": 2,
+    Memoria: 300,
+    "TD + Memoria": 2,
+    Reconocimiento: 80,
+    Random: 90,
+  },
+  {
+    "Toma de Decisión": 5,
+    Memoria: 225,
+    "TD + Memoria": 3,
+    Reconocimiento: 60,
+    Random: 120,
+  },
+];
--- components/statistcs/RadarChart.tsx
+++ components/statistcs/RadarChart.tsx
@@ -1,29 +1,61 @@
-import React, { useState } from "react";
+import React from "react";
+import { G, Rect } from "react-native-svg";
 import {
   VictoryArea,
   VictoryChart,
   VictoryGroup,
   VictoryLabel,
   VictoryPolarAxis,
+  VictoryScatter,
   VictoryTheme,
 } from "victory-native";
 
+type Data = Record<string, number>[];
+
 const characterData = [
-  { strength: 1, intelligence: 250, luck: 1, stealth: 40, charisma: 50 },
-  { strength: 2, intelligence: 300, luck: 2, stealth: 80, charisma: 90 },
-  { strength: 5, intelligence: 225, luck: 3, stealth: 60, charisma: 120 },
+  {
+    "Toma de Decisión": 2,
+    Memoria: 300,
+    "TD + Memoria": 2,
+    Reconocimiento: 80,
+    Random: 90,
+  },
+  {
+    "Toma de Decisión": 5,
+    Memoria: 225,
+    "TD + Memoria": 3,
+    Reconocimiento: 60,
+    Random: 120,
+  },
 ];
 
-export function RadarChart() {
-  const [state, setState] = useState({
+type Props = {
+  labels: string[];
+  characterData: Data;
+};
+
+export function RadarChart({ labels, characterData }: Props) {
+  const state = {
     data: processData(characterData),
     maxima: getMaxima(characterData),
-  });
+  };
 
   return (
-    <VictoryChart polar theme={VictoryTheme.material} domain={{ y: [0, 1] }}>
+    <VictoryChart
+      polar
+      theme={VictoryTheme.material}
+      domain={{ y: [0, 1] }}
+      height={350}
+      style={{
+        parent: {
+          paddingTop: 10,
+          paddingBottom: 10,
+          transform: `rotate(-${90 / 5}deg)`,
+        },
+      }}
+    >
       <VictoryGroup
-        colorScale={["gold", "orange", "tomato"]}
+        colorScale={["#58DAFC", "#FF6622", "tomato"]}
         style={{ data: { fillOpacity: 0.2, strokeWidth: 2 } }}
       >
         {state.data.map((data, i) => {
@@ -36,7 +68,10 @@ export function RadarChart() {
             key={i}
             dependentAxis
             style={{
-              axisLabel: { padding: 10 },
+              axisLabel: {
+                padding: 40,
+                fontWeight: "bold",
+              },
               axis: { stroke: "none" },
               grid: { stroke: "grey", strokeWidth: 0.25, opacity: 0.5 },
             }}
@@ -44,8 +79,7 @@ export function RadarChart() {
             labelPlacement="perpendicular"
             axisValue={i + 1}
             label={key}
-            tickFormat={(t) => Math.ceil(t * state.maxima[key])}
-            tickValues={[0.25, 0.5, 0.75]}
+            tickFormat={(t) => ""}
           />
         );
       })}
@@ -54,30 +88,82 @@ export function RadarChart() {
         tickFormat={() => ""}
         style={{
           axis: { stroke: "none" },
-          grid: { stroke: "grey", opacity: 0.5 },
+          grid: { stroke: "grey", opacity: 0.8 },
         }}
+        tickLabelComponent={<VictoryLabel labelPlacement="vertical" />}
       />
+      {Object.entries(state.data[0]).map(([key, value], i) => (
+        <VictoryScatter
+          key={i}
+          data={[
+            {
+              x: i + 1,
+              y: value.y,
+            },
+          ]}
+          size={6}
+          style={{
+            data: { fill: "#58DAFC" },
+            labels: {
+              fill: "black",
+              fontSize: 10,
+              padding: 18,
+              backgroundColor: "#ff0000",
+            },
+          }}
+          labelComponent={<CustomLabelWithBackground />}
+          labels={labels}
+        />
+      ))}
     </VictoryChart>
   );
 }
 
-function getMaxima(data) {
-  const groupedData = Object.keys(data[0]).reduce((memo, key) => {
+function getMaxima(data: Data) {
+  const groupedData = Object.keys(data[0]).reduce((memo: any, key) => {
     memo[key] = data.map((d) => d[key]);
     return memo;
   }, {});
-  return Object.keys(groupedData).reduce((memo, key) => {
+  return Object.keys(groupedData).reduce((memo: any, key) => {
     memo[key] = Math.max(...groupedData[key]);
     return memo;
   }, {});
 }
 
-function processData(data) {
+function processData(data: Data) {
   const maxByGroup = getMaxima(data);
-  const makeDataArray = (d) => {
+  const makeDataArray = (d: any) => {
     return Object.keys(d).map((key) => {
       return { x: key, y: d[key] / maxByGroup[key] };
     });
   };
   return data.map((datum) => makeDataArray(datum));
 }
+
+const CustomLabelWithBackground = (props: any) => {
+  const { x, y, text, datum } = props;
+  const angles = [90, 90 / 5, -(3 * 90) / 5, (3 * 90) / 5, -90 / 5];
+  const angle = angles[datum._x - 1];
+
+  return (
+    <G transform={`rotate(${angle}, ${x}, ${y})`}>
+      <Rect
+        x={x - 60}
+        y={y - 8}
+        width={120}
+        height={16}
+        fill="#ABEDFD"
+        stroke="#ABEDFD"
+        strokeWidth={1}
+        rx={8}
+        ry={8}
+      />
+      <VictoryLabel
+        {...props}
+        datum={datum}
+        labelPlacement="vertical"
+        verticalAnchor="middle"
+      />
+    </G>
+  );
+};

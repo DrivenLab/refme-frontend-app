1be51197dd8617dbea6e3d0e8c3522d5caa88750
David <nundavid99@gmail.com>
2024-05-24 23:33:19 -0400

    styling in session result bar chart

--- components/workouts/SessionResultBarChart.tsx
+++ components/workouts/SessionResultBarChart.tsx
@@ -5,7 +5,6 @@ import {
   VictoryAxis,
   VictoryBar,
   VictoryChart,
-  VictoryLabel,
   VictoryStack,
   VictoryTheme,
 } from "victory-native";
@@ -15,6 +14,11 @@ const SessionResultBarChart = ({ session }: Props) => {
   const data = session.iterations.map((iteration, index) => ({
     x: index + 1,
     y: iteration.answeredInMs / 1000,
+    iteration,
+    hasVideo: iteration.video ? true : false,
+    answerIsCorrect:
+      iteration.answer1 === iteration.userAnswer1 &&
+      iteration.answer2 === iteration.userAnswer2,
   }));
   const colors = session.iterations.map(
     (iteration) => RPE_COLORS[`${iteration.rpe}` as keyof typeof RPE_COLORS]
@@ -23,18 +27,24 @@ const SessionResultBarChart = ({ session }: Props) => {
     <Box borderWidth={1} margin={10} borderColor="#a1a1a1" borderRadius={7}>
       <VictoryChart theme={VictoryTheme.material} height={220} width={500}>
         <VictoryStack colorScale={colors} domain={{ x: [0, 8], y: [0, 7] }}>
-          {data.map(({ x, y }, i) => {
-            return (
-              <VictoryBar
-                key={i}
-                data={[{ x, y: y }]}
-                barWidth={15}
-                labels={({ datum }) => (datum.x == x ? `${data[i].x}%` : "")}
-                labelComponent={<VictoryLabel y={380} />}
-                cornerRadius={5}
-              />
-            );
-          })}
+          <VictoryBar
+            animate={{ duration: 1000, onLoad: { duration: 500 } }}
+            data={data}
+            barWidth={15}
+            style={{
+              data: {
+                fill: ({ datum }) => {
+                  return RPE_COLORS[
+                    datum.iteration.rpe as keyof typeof RPE_COLORS
+                  ];
+                },
+              },
+            }}
+            labels={({ datum }) =>
+              !datum.hasVideo ? "" : datum.answerIsCorrect ? "✅" : "❌"
+            }
+            cornerRadius={5}
+          />
 
           <VictoryAxis dependentAxis tickFormat={(t) => `${t}s`} />
           <VictoryAxis tickFormat={(t) => `HIT ${t}`} />

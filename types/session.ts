35a98fffb06546a29eb19a684dcbbe37bb083b50
David <nundavid99@gmail.com>
2024-06-05 17:29:35 -0400

    types & removing unneeded file

--- types/session.ts
+++ types/session.ts
@@ -12,7 +12,7 @@ export interface Session {
   workoutId: number;
   userIterationAnswer: any[];
 }
-type IterationWorkout = {
+export type IterationWorkout = {
   idIteration: number;
   video?: string;
   answeredInMs: number;
@@ -177,7 +177,8 @@ export type MEMORY_STEPS =
   | "workout"
   | "video"
   | "decision"
-  | "rpe";
+  | "rpe"
+  | "beginMemoryWorkout";
 
 export type VideoAnswerDonwload = {
   uri1?: string;

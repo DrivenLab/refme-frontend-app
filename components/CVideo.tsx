e74ded15cc2bebd91812927723672c17e93079c0
Fer Rios <luis.fer334.fa@gmail.com>
2024-06-11 18:09:20 -0400

    chore: Refactor CVideo component to use react-use-precision-timer

--- components/CVideo.tsx
+++ components/CVideo.tsx
@@ -1,7 +1,9 @@
+import { useEffect, useRef } from "react";
 import { StyleSheet, StyleProp, ViewStyle } from "react-native";
-import { useEffect, useRef, useState } from "react";
-import { AVPlaybackStatus, ResizeMode, Video } from "expo-av";
+import { ResizeMode, Video } from "expo-av";
+import { useDelay } from "react-use-precision-timer";
 import { View } from "@gluestack-ui/themed";
+
 type Props = {
   uri: string;
   isLooping?: boolean;
@@ -9,55 +11,37 @@ type Props = {
   onFinishVideo: () => void;
 };
 const CVideo = ({ uri, isLooping, customStyles, onFinishVideo }: Props) => {
-  const video = useRef<any>(null);
-  const [status, setStatus] = useState<AVPlaybackStatus>();
-  useEffect(() => {}, []);
-  const handleOnPress = () => {
-    if (!video.current) return;
-    if (status?.isLoaded && status.isPlaying) video.current.pauseAsync();
-    else video.current.playAsync();
-  };
-  const onPlaybackStatusUpdate = (status: AVPlaybackStatus) => {
-    if (status.isLoaded && status.didJustFinish) {
-      onFinishVideo();
-      // Handle the event when the video finishes
+  const video = useRef<Video>(null);
+
+  const onceTimer = useDelay(10 * 1000, () => onFinishVideo());
+  useEffect(() => {
+    onceTimer.start();
+  }, []);
+
+  const handleLoad = async (status: any) => {
+    if (status.durationMillis) {
+      const duration = status.durationMillis;
+      const desiredDuration = 10 * 1000;
+      const speedFactor = duration / desiredDuration;
+
+      await video.current!.setRateAsync(speedFactor, false);
+      await video.current!.playAsync();
     }
   };
+
   return (
     <View flex={1}>
       <Video
         ref={video}
         source={{ uri }}
-        rate={1.0}
         volume={1.0}
         isMuted={true}
         resizeMode={ResizeMode.COVER}
-        shouldPlay
+        onLoad={handleLoad}
         isLooping={isLooping}
-        onPlaybackStatusUpdate={onPlaybackStatusUpdate}
         style={[{ width: "100%", height: "100%" }, customStyles]}
       />
     </View>
   );
 };
-const styles = StyleSheet.create({
-  contentContainer: {
-    flex: 1,
-    padding: 10,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingHorizontal: 50,
-  },
-  video: {
-    width: 350,
-    height: 275,
-  },
-  controlsContainer: {
-    padding: 10,
-  },
-  buttons: {
-    width: 100,
-    height: 100,
-  },
-});
 export default CVideo;
